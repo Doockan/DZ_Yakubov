2e0ef9bbba0db97d4476e782a9f1a8f13bb83c00
Doockan <avazyakubow@gmail.com>
2021-12-14 16:29:29 +0400

    first commit

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    void Start()
+    {
+        var e01 = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float movementSpeed;
+    public float turningSpeed;
+    public float force; 
+    bool is_ground = false;
+    Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        /*float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);*/
+
+        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+
+        if (Input.GetKey(KeyCode.Space) && is_ground)
+        {
+            player.AddForce(Vector3.up * force);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            is_ground = true;
+            Debug.Log(is_ground);
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            is_ground = false;
+            Debug.Log(is_ground);
+        }
+    }
+}
